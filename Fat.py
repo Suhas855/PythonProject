b098022726400b34462ba1d2bceebe559b4e1a38
Suhas Kotha <suhas_kotha@hotmail.com>
2021-06-23 21:09:28 -0500

    Weight caculator

--- Fat.py
+++ Fat.py
@@ -1,13 +1,14 @@
 Weight = input("How much do you weigh: ")
 Units = input("Kg or Lb: ")
-lb = 2.20462262 * int(Weight)
-rounded_lb = round(2.20462262 * int(Weight))
-kg = int(Weight) / 2.20462262
-rounded_kg = round(int(Weight) / 2.20462262)
+
 if Units == "K" or Units == "k":
+    lb = 2.20462262 * int(Weight)
+    rounded_lb = round(2.20462262 * int(Weight))
     print("Your weight is " + Weight + " kgs")
-    print("Your weight is " + str(lb) + "lbs or " + str(rounded_lb))
+    print("Your weight is " + str(lb) + " lbs or " + str(rounded_lb))
 elif Units == "L" or Units == "l":
+    kg = int(Weight) / 2.20462262
+    rounded_kg = round(int(Weight) / 2.20462262)
     print("Your weight is " + Weight + " lbs")
     print("Your weight is " + str(kg) + " kgs or " + str(rounded_kg))
 else:
